9aa1158950a13f0e9cec6f3e2b2fab1f8b3e32db
Eric Yu <eric.yu@snorkel.ai>
2021-11-02 18:46:22 +0000

    Set up Stack Status app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.png";
+import "./App.css";
 
-function App() {
+let GITHUB_PERSONAL_ACCESS_TOKEN: string | null;
+try {
+  GITHUB_PERSONAL_ACCESS_TOKEN = require("./github_personal_access_token.json");
+} catch {
+  GITHUB_PERSONAL_ACCESS_TOKEN = null;
+}
+
+function App(): JSX.Element {
+  if (GITHUB_PERSONAL_ACCESS_TOKEN == null) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Please create a{" "}
+            <a href="https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token">
+              GitHub Personal Access Token
+            </a>{" "}
+            and then create <code>src/github_personal_access_token.json</code>{" "}
+            with your token in double quotes.
+          </p>
+        </header>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{GITHUB_PERSONAL_ACCESS_TOKEN}</p>
       </header>
     </div>
   );
